4d7409980a01a3c12f84841de1dc10afb41f4046
AJGrant10 <ajgrant1@student.fullsail.edu>
2019-01-14 22:22:07 -0500

    add Menu challenge

---
+++ challenges/1-Menu/DVP1.CE1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DVP1.CE1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Coding challenge menu:");
+                Console.ReadLine();
+
+                Console.WriteLine("[1]Swap Name");
+                Console.WriteLine("[2] Backwards");
+                Console.WriteLine("[3] Age Convert");
+                Console.WriteLine("[4] Temp Convert");
+                Console.WriteLine("[5] Big Blue Fish");
+
+                Console.WriteLine("Please enter the number for the challenge you want to run...");
+            }
+        }
+    }
+}
