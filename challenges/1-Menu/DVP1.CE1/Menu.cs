c04baaf86bb9ca08cfc0d48b855af8c2ff8133f4
AJGrant10 <ajgrant1@student.fullsail.edu>
2019-01-16 22:35:10 -0500

    Fixed menu challenge

--- challenges/1-Menu/DVP1.CE1/Menu.cs
+++ challenges/1-Menu/DVP1.CE1/Menu.cs
@@ -3,37 +3,96 @@
 //2019 January
 //Project & Portfolio 1
 //Menu Challenge
+//Fixed menu challenge In this challenge I created a welcome menu
 namespace DVP1.CE1
 {
     class MainClass
     {
         public class  Main(string[] args)
         {
-            {
-                Console.WriteLine("Coding challenge menu:");
-                Console.ReadLine();
+           
+    class Menu
+    {
+        static int menuChoice = 0;
 
-                Console.WriteLine("[1]Swap Name");
+
+        public static void GetMenu()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Menu");
+                Console.WriteLine("Enter listed number choice");
+                Console.WriteLine("");
+                Console.WriteLine("[1] Swap Name");
                 Console.WriteLine("[2] Backwards");
                 Console.WriteLine("[3] Age Convert");
                 Console.WriteLine("[4] Temp Convert");
                 Console.WriteLine("[5] Big Blue Fish");
+                Console.WriteLine("");
+                Console.WriteLine("[0] Exit");
+                Console.WriteLine("");
+                Console.WriteLine("Enter your selected challenge");
 
-                Console.WriteLine("Please enter the number for the challenge you want to run...");
+                switch (GetMenu(menuChoice))
+                {
+                    case 1:
+                        {
+                            Console.Clear();
+                            SwapName.GetSwapName();
+                        }
+                        break;
+                    case 2:
+                        {
+                            Console.Clear();
+                            Backwards.GetBackWards();
+                        }
+                        break;
+                    case 3:
+                        {
+                            Console.Clear();
+                            AgeConvert.GetAgeConvert();
+                        }
+                        break;
+                    case 4:
+                        {
+                            Console.Clear();
+                            TempConvert.GetTempConvert();
+                        }
+                        break;
+                    case 5:
+                        {
+                            Console.Clear();
+                            BigBlueFish.GetBigBlueFish();
+                        }
+                        break;
+                    case 0:
+                        {
+                            if (menuChoice == 0)
+                            {
+                                Environment.Exit(0);
+                            }
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+                
             }
-            
-            public static void second(string[] args)
-        {
-            
-            Console.WriteLine("Welcome to SwapName:");
-            Console.WriteLine("To begin enter your first name...");
-            string inputText = Console.ReadLine();
-
-            Console.WriteLine("Thank you " + inputText + " ,now enter your last name");
-            string inputText2 = Console.ReadLine();
-            Console.WriteLine("Excellent! Your name is " + inputText + " "+ inputText2 +" reversed would be "+ inputText2 +" " + inputText);
-            Console.ReadLine();
         }
+
+        public static int GetMenu(int menuChoiceInt)
+        {
+            string menuInput = Console.ReadLine();
+
+            while (!int.TryParse(menuInput, out menuChoiceInt) && menuChoiceInt < 0 && menuChoiceInt > 5)
+            {
+                Console.WriteLine("Please enter one of the choices in the menu");
+            }
+            return menuChoiceInt;
         }
     }
-}
+} 
+        
+
