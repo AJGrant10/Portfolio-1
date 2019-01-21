9309c676d014e53f370c465cad05ef3b24d51ac6
AJGrant10 <ajgrant1@student.fullsail.edu>
2019-01-20 20:09:50 -0500

    Swap name challenge

---
+++ challenges/1-Menu/DVP1.CE1/SwapName.cs
@@ -0,0 +1,21 @@
+namespace DVP1.CE1
+{
+    public class SwapName
+    {
+        namespace SwapName
+{
+    public static void GetSwapName()
+    
+        {
+
+            Console.WriteLine("Welcome to SwapName:");
+            Console.WriteLine("To begin enter your first name...");
+            string inputText = Console.ReadLine();
+
+            Console.WriteLine("Thank you " + inputText + " ,now enter your last name");
+            string inputText2 = Console.ReadLine();
+            Console.WriteLine("Excellent! Your name is " + inputText + " "+ inputText2 +" reversed would be "+ inputText2 +" " + inputText);
+            Console.ReadLine();
+        }
+    }
+}
